hhvm/hack-mode

commit ccb20c2dac594c6d2d46d5d0555c428a7d0aa76e

Treat < and > as delimiters

hack-mode.el
@@ -324,6 +324,10 @@ See <http://php.net/manual/en/language.types.string.php>."
     (modify-syntax-entry ?\n "> b" table)
     (modify-syntax-entry ?\^m "> b" table)
 
+    ;; < and > are paired delimiters.
+    (modify-syntax-entry ?< "(>" table)
+    (modify-syntax-entry ?> ")<" table)
+
     table))
 
 (defun hack--comment-prefix (s)

commit e3a231e4285be6bee11da3ea1486d55f15645f41

Fix < and > being treated as paired delimiters in the wrong context
No only did this break highlighting of the corresponding pair, but it
had also broken highlighting.

This bug was introduced in ccb20c2.

hack-mode.el
@@ -79,22 +79,38 @@ See <http://php.net/manual/en/language.types.string.php>."
       (seq "#!" (* not-newline) "\n"))
      (group "<?hh"))))
 
-(defun hack--propertize-header ()
-  "Ensure <?hh is not treated as a < delimiter."
-  (let ((start (match-beginning 1))
-        (end (match-end 1)))
-    (put-text-property start end
-		       'syntax-table (string-to-syntax "."))))
+(defun hack--propertize-lt ()
+  "Ensure < is not treated a < delimiter in other syntactic contexts."
+  (let ((start (1- (point))))
+    (when (or (looking-at "?hh")
+              ;; If there's a following space, assume it's 1 < 2.
+              (looking-at " "))
+      (put-text-property start (1+ start)
+		         'syntax-table (string-to-syntax ".")))))
+
+(defun hack--propertize-gt ()
+  "Ensure > in -> or => isn't treated as a > delimiter."
+  (let* ((start (1- (point))))
+    (when (> start (point-min))
+      (let ((prev-char (char-before start)))
+        ;; If there's a preceding space, we assume it's 1 < 2 rather than
+        ;; a type vec < int > with excess space.
+        (when (memq prev-char (list ?= ?- ?\ ))
+          (put-text-property start (1+ start)
+		             'syntax-table (string-to-syntax ".")))))))
 
 (defconst hack--syntax-propertize-function
   (syntax-propertize-rules
-   (hack--header-regex
-    (0 (ignore (hack--propertize-header))))
-   ;; Heredocs, of the form <<<EOT
+   ;; Heredocs, e.g.
+   ;; $x = <<<EOT
    ;; foo bar
-   ;; EOT
+   ;; EOT;
    ("<<<"
-    (0 (ignore (hack--propertize-heredoc))))))
+    (0 (ignore (hack--propertize-heredoc))))
+   ("<"
+    (0 (ignore (hack--propertize-lt))))
+   (">"
+    (0 (ignore (hack--propertize-gt))))))
 
 (defvar hack-font-lock-keywords
   `(

test/hack-unit-test.el
@@ -144,6 +144,60 @@ then run BODY."
          (font-lock-fontify-buffer)))
      ,@body))
 
+(ert-deftest hack-syntax-angle-bracket-for-type ()
+  "Match angle brackets in type parameters."
+  (with-hack-buffer "function foo(vec<int> $_): void {}"
+    (search-forward ">")
+    (backward-char)
+    (should
+     (eq (syntax-class (syntax-after (point)))
+         5))))
+
+(ert-deftest hack-syntax-angle-bracket-method ()
+  "Method access is not a matched pair."
+  (with-hack-buffer "$foo->bar();"
+    (search-forward ">")
+    (backward-char)
+    (should
+     (eq (syntax-class (syntax-after (point)))
+         1))))
+
+(ert-deftest hack-syntax-angle-bracket-lambda ()
+  "Lambdas are not a matched pair."
+  (with-hack-buffer "() ==> $x;"
+    (search-forward ">")
+    (backward-char)
+    (should
+     (eq (syntax-class (syntax-after (point)))
+         1))))
+
+(ert-deftest hack-syntax-angle-bracket-dict ()
+  "Lambdas are not a matched pair."
+  (with-hack-buffer "dict['foo' => 42];"
+    (search-forward ">")
+    (backward-char)
+    (should
+     (eq (syntax-class (syntax-after (point)))
+         1))))
+
+(ert-deftest hack-syntax-angle-bracket-less-than ()
+  "Less than is not a matched type delimiter."
+  (with-hack-buffer "$x = 1 > 2;"
+    (search-forward ">")
+    (backward-char)
+    (should
+     (eq (syntax-class (syntax-after (point)))
+         1))))
+
+(ert-deftest hack-syntax-angle-bracket-greater-than ()
+  "Greater than is not a matched type delimiter."
+  (with-hack-buffer "$x = 1 < 2;"
+    (search-forward "<")
+    (backward-char)
+    (should
+     (eq (syntax-class (syntax-after (point)))
+         1))))
+
 (ert-deftest hack-highlight-header ()
   (with-hack-buffer "<?hh // strict"
     (search-forward "h")